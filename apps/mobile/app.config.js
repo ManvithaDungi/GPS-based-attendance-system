eda70008a3e5b785aeceafa6c47f7439737a9cd9
Manvitha Dungi <manvitha3626@gmail.com>
2026-05-06 23:20:36 +0530

    change workflow

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,7 +1,11 @@
 const appJson = require('./app.json');
 
+
+
 module.exports = () => ({
   ...appJson.expo,
+  owner: "manvithadungi",
+  slug: "gpsattendancesystem",
   extra: {
     ...appJson.expo.extra,
     apiUrl: process.env.EXPO_PUBLIC_API_URL || appJson.expo.extra?.apiUrl,
